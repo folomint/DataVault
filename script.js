257b88ba5b2daaca03fd6a18e0c74e89c1e628e3
folomint <folomintsmm@gmail.com>
2026-01-19 04:30:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function notPaid(){
+ alert("Payment not received yet. Please complete payment first.");
+}
+function verify(){
+ alert("UTR received. Please wait 15-30 minutes for verification.");
+}
